d489f199dbaf3f035493b0704c8a494822acf1ee
Lizzza <lizazhyvitskaya@gmail.com>
2022-07-31 10:59:01 +0300

    change callback on promise

--- db/db.js
+++ db/db.js
@@ -7,16 +7,13 @@ const connection = mysql.createConnection(connectionUri);
 executeQuery(createAuthorTable);
 executeQuery(createBookTable);
 
-function executeQuery(query) {
-    return new Promise( (resolve, reject) => {
-        connection.query(query, (err, rows, fields) => {
-            if (err) {
-                console.log(err);
-                return reject(err);
-            }
-            return resolve(rows);
-        });
-    });
+async function executeQuery(query) {
+    try {
+        const res = await connection.promise().query(query);
+        return res[0];
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = { executeQuery };
