a43bc022b7286ed7a63b6905f6254e79261040c9
Lizzza <lizazhyvitskaya@gmail.com>
2022-08-23 20:08:01 +0300

    delete js

--- routes/home.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.render('home');
-});
-module.exports = router;
